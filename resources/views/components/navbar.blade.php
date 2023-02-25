bbce63d9b9e847c5294a3c3478a6789f1c22d3a6
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2023-02-25 20:51:51 +0800

    mengganti Logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Ayani Dental Care</a>
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('/static') }}/AYANI.png" height="30" alt="">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
